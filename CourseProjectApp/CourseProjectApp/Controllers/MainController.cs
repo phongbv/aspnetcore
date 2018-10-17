ab8dce2769ee07a4d81c4fc0a4ac9ed3a02a173f
PhongBV <buivan.phong94@gmail.com>
2018-10-17 17:23:45 +0700

    Cr

---
+++ CourseProjectApp/CourseProjectApp/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseProjectApp.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.value = "My first MVC Route!!!!!";
+            return View();
+        }
+    }
+}
